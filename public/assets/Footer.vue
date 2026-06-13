ddc7af10d18a4e0da0f50759105dd22013c3893a
星觅海 <1498934815@qq.com>
2026-06-13 08:21:56 +0800

    更新 Footer.vue

--- public/assets/Footer.vue
+++ public/assets/Footer.vue
@@ -20,11 +20,11 @@
     </div>
 
     <div class="project-links">
-      <strong>项目:</strong><a href="https://github.com/willow-god/FlareDrive-R2" target="_blank"
-        rel="noopener noreferrer">FlareDrive-R2</a>
-      <strong>作者:</strong><a href="https://github.com/willow-god" target="_blank"
+      <strong>项目:</strong><a href="https://github.com/xingmihai/FlareDrive-R2-Workers" target="_blank"
+        rel="noopener noreferrer">FlareDrive-R2-Workers</a>
+      <strong>作者:</strong><a href="https://github.com/xingmihai" target="_blank"
         rel="noopener noreferrer">LiuShen</a>
-      <strong>初始项目:</strong><a href="https://github.com/ljxi/Cloudflare-R2-oss" target="_blank"
+      <strong>初始项目:</strong><a href="https://github.com/willow-god/FlareDrive-R2" target="_blank"
         rel="noopener noreferrer">Cloudflare-R2-OSS</a>
     </div>
 
@@ -40,9 +40,9 @@ export default {
   name: "Footer",
   data() {
     return {
-      homeUrl: "https://www.liushen.fun/",
-      blogUrl: "https://blog.liushen.fun/",
-      githubUrl: "https://github.com/willow-god",
+      homeUrl: "https://page.xmhai.cn",
+      blogUrl: "https://www.xmhai.cn",
+      githubUrl: "https://github.com/xingmihai",
       emailUrl: "mailto:01@liushen.fun"
     };
   }
